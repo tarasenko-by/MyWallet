a7ffb9669161e4248ad824b19e2cffc5328dc723
Tarasenko <tarasenko-by@yandex.ru>
2021-12-27 22:11:29 +0400

    Initial Commit

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by Sergei Tarasenko on 27.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
